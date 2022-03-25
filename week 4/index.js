d653578641552cf60cdad91116ce77787ea2ff83
Michelle Earl <michellevearl@gmail.com>
2022-03-25 10:21:48 -0600

    adding week 1-4

---
+++ week 4/index.js
@@ -0,0 +1,77 @@
+console.log("Here is my first console log!")
+
+let clothes = 'shirt';
+
+const mathProblem = '2+2=4';
+
+console.log("She said \"STOP!!!\" \n He said \"GO!!!\"");
+
+console.log(106);
+console.log(-45);
+console.log(1.5);
+
+let myBoolean = true;
+
+let isWarmOut = false;
+
+let dinnerTonight;
+
+
+let anything = null;
+
+let me = {
+    name: 'Michelle',
+    pronouns: 'she/her',
+    race: 'Ecuadorian',
+    isTall: false,
+    age: 34.95,
+
+};
+
+console.log(me.name);
+
+console.log(me);
+
+console.log(me['age']);
+
+let color = "blue;"
+
+console.log(typeof color);
+
+color = false;
+
+console.log(typeof color);
+
+function greeting() {
+    console.log('HELLO!!');
+}
+
+greeting();
+
+function greeting2(greeting) {
+    console.log(greeting);
+}
+
+
+greeting2("Hola!!!")
+greeting2("34")
+greeting2("1.2")
+
+
+
+function sayHello(greeting, firstName, lastName) {
+    console.log(greeting, firstName, lastName);
+}
+
+sayHello('Hola!', 'Michelle', 'Earl');
+
+
+
+function animalNames(firstPet, secondPet) {
+    console.log(firstPet, secondPet);
+}
+
+animalNames('Sammy', 'Kassie');
+
+
+
